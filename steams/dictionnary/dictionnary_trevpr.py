a3839cb3062ccf09ba7f73181ef90a9da6aa063e
jml <jml@nilu.no>
2022-05-12 09:07:58 +0200

    typo

--- steams/dictionnary/dictionnary_trevpr.py
+++ steams/dictionnary/dictionnary_trevpr.py
@@ -1,17 +1,22 @@
 from steams.models.attention import single_train_attention, loss_attention, evaluation_bytarget_attention, predict_attention
 
 train_dict = {
-    "Ssteams": single_train_Ssteams
+    "ED_NW_att": single_train_attention,
+    "multi_head_att": single_train_attention
+
 }
 
 loss_dict = {
-    "Ssteams": loss_Ssteams
+    "ED_NW_att": loss_attention,
+    "multi_head_att": loss_attention
 }
 
 eval_bytarget_dict = {
-    "Ssteams": evaluation_bytarget_Ssteams
+    "ED_NW_att": evaluation_bytarget_attention,
+    "multi_head_att": evaluation_bytarget_attention
 }
 
 predict_dict = {
-    "Ssteams": predict_Ssteams
+    "ED_NW_att": predict_attention,
+    "multi_head_att": predict_attention
 }
